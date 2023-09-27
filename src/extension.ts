8f580ed4501e5a3b44acdb49ca8593bd0cb198a9
stagas <gstagas@gmail.com>
2023-09-27 20:27:35 +0300

    Add cyclicExpand.prev

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ export const cyclicExpand = new CyclicExpand()
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand('cyclicExpand.next', cyclicExpand.next),
+    vscode.commands.registerTextEditorCommand('cyclicExpand.prev', cyclicExpand.prev),
     vscode.window.onDidChangeTextEditorSelection(cyclicExpand.reset),
     vscode.workspace.onDidChangeTextDocument(cyclicExpand.reset),
   )
